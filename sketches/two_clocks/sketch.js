cebf2c232b1cc4ea59c9e07d75a7395309244f51
JunShern <chanjunshern@gmail.com>
2018-08-02 10:00:43 +0800

    Finish up Part 1

--- sketches/two_clocks/sketch.js
+++ sketches/two_clocks/sketch.js
@@ -15,43 +15,26 @@ function setup() {
 }
 
 function draw() {
-  if (sloop.isPlaying) {
-    text('click to pause', width/2, height/2);
-
-    fill(255,100,100);
-    rect(0, 0, width/2, height);
-    fill(100,100,255);
-    rect(width/2, 0, width/2, height);
-    
-    synth1.play(440, 0.7, 0, 0.5);
-    counter1++;
-    fill(0);
-    
-    textSize(min(width, height)/20);
-    text("Draw loop", width*1/4, height/10);
-    text("SoundLoop", width*3/4, height/10);
-    textSize(min(width, height)/5);
-    text(counter1, width*1/4, height/2);
-    text(counter2, width*3/4, height/2);
+  fill(255,100,100);
+  rect(0, 0, width/2, height);
+  fill(100,100,255);
+  rect(width/2, 0, width/2, height);
   
-  } else {
-    background(200, 0.7);
-    text('click to play', width/2, height/2);
+  if (sloop.isPlaying) {
+    synth1.play('C4', 0.7, 0, 0.5);
+    counter1++;
   }
 
+  fill(0);
+  textSize(min(width, height)/20);
+  text("Draw loop (Low pitch)", width*1/4, height/10);
+  text("SoundLoop (High pitch)", width*3/4, height/10);
+  textSize(min(width, height)/5);
+  text(counter1, width*1/4, height/2);
+  text(counter2, width*3/4, height/2);
 }
 
 function mySoundLoop() {
-  synth2.play(880, 0.7, 0, 0.5);
+  synth2.play('E4', 0.7, 0, 0.5);
   counter2++;
-}
-
-function touchStarted() {
-  if (sloop.isPlaying) {
-    sloop.pause();
-    noLoop();
-  } else {
-    sloop.start();
-    loop();
-  }
 }
